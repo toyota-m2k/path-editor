e2fe70415c5605751d0fae12fd061d7d78c759d1
toyota-m2k <toyota.mitsuki@gmail.com>
2024-02-06 12:37:11 +0900

    feat: タイトルバーにバージョン番号、debug版かどうかを表示

--- PathEdit/MainWindow.xaml.cs
+++ PathEdit/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 using Microsoft.UI.Xaml;
+using PathEdit.common;
 using System;
 using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -12,8 +15,26 @@ namespace PathEdit {
     public sealed partial class MainWindow : Window {
 
         public MainWindow() {
+            string AppName;
+            Version AppVersion;
             this.InitializeComponent();
-            this.Title = "SVG Path Editor";
+            if (RuntimeHelper.IsMSIX) {
+                var package = Windows.ApplicationModel.Package.Current;
+                AppName = package.DisplayName;
+                var v = package.Id.Version;
+                AppVersion = new(v.Major, v.Minor, v.Build, v.Revision);
+            }
+            else {
+                var assemblyName = Assembly.GetExecutingAssembly().GetName();
+                AppName = assemblyName.Name ?? "who am i?";
+                AppVersion = assemblyName.Version ?? new Version(0, 0, 0, 0);
+            }
+            var title = $"{AppName} v{AppVersion}";
+
+#if DEBUG
+            title += " <debug>";
+#endif
+            this.Title = title;
             this.AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/program-icon.ico"));
         }
 
